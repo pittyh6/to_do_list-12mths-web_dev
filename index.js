8450d7ea890dda87d9131ac463f3633b99d82903
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-03-27 14:14:23 +1100

    fix name variables button

---
+++ index.js
@@ -0,0 +1,8 @@
+let input_field = $(".input-text")
+console.log(input_field)
+
+
+const add_btn = document.getElementByClass("add-btn");
+add_btn.addEventListener("click",function() {
+   alert("hi")
+})

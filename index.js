dbaf18391a4e43a016d261b276092242f437266d
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-03-27 14:55:30 +1100

    add text to the div that was wrote in input

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let input_field = $(".input-text")
 let add_btn = $(".add-btn")
-console.log(add_btn)
+let show_item_listed = $(".box-item")
 
 
 
 // get text inserted on input_field
-function addItemList(){
+function getTextItemList(){
     let text = input_field.val()
     console.log(text)
+    showItemList(text)
+}
+function showItemList(text){
+    console.log(text + " on show")
+    show_item_listed.text(text)
 }

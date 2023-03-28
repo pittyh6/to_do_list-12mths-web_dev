0a87c4af4792c7e0886cf1e5d80a037bc73f2174
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-03-28 13:56:40 +1100

    fix bug multiplos item list created

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ let get_item_div = $(".items")
 
 
 // get text inserted on input_field
-function getTextItemList(){
+function getTextItemList() {
     let text = input_field.val()
     //addTextList(text)
     showItemList(text)
 }
-function addTextList(text, box_item){
+function addTextList(text, box_item) {
     let getText = text
-    showItemList()
     show_item_listed.text(getText)
     box_item.text(getText)
 }
 
-function showItemList(text){
+function showItemList(text) {
     // Create Item div to put items in separate block
     let item_list = document.createElement('div')
     item_list.className = 'item'
     get_item_div.append(item_list)
-
+    // Create box to add text item list
     let box_item = document.createElement('div');
     box_item.className = 'box-item'
     item_list.append(box_item);
+    // Create button delete
+
 
     addTextList(text, box_item)
 }

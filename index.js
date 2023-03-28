9027ceb2798919c716390e5590957ed40219a3a9
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-03-28 13:44:21 +1100

    create html element onClick js

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 let input_field = $(".input-text")
 let add_btn = $(".add-btn")
 let show_item_listed = $(".box-item")
-
+let get_item_div = $(".items")
 
 
 // get text inserted on input_field
 function getTextItemList(){
     let text = input_field.val()
-    console.log(text)
+    //addTextList(text)
     showItemList(text)
 }
+function addTextList(text, box_item){
+    let getText = text
+    showItemList()
+    show_item_listed.text(getText)
+    box_item.text(getText)
+}
+
 function showItemList(text){
-    console.log(text + " on show")
-    show_item_listed.text(text)
+    // Create Item div to put items in separate block
+    let item_list = document.createElement('div')
+    item_list.className = 'item'
+    get_item_div.append(item_list)
+
+    let box_item = document.createElement('div');
+    box_item.className = 'box-item'
+    item_list.append(box_item);
+
+    addTextList(text, box_item)
 }

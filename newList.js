be74590a46a7a9a6cafd77dbf7553decea583d62
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-04-05 13:44:08 +1000

    add action show and hide when click on btn create new list

--- newList.js
+++ newList.js
@@ -1,5 +1,19 @@
 let create_new_list_btn = $('.new-list')
+let input_text_create_list = $('.input-list-name')
+let insert_list_name = $('.insert-list-name')
 
 create_new_list_btn.click(function(){
-    window.prompt("sometext","defaultText");
+    if(insert_list_name.hasClass('hide')){
+        insert_list_name.removeClass('hide')
+        insert_list_name.addClass('show')
+    }else{
+        insert_list_name.removeClass('show')
+        insert_list_name.addClass('hide')
+    }
 })
+
+
+function createList(){
+    let list_name = input_text_create_list.val()
+    console.log(list_name)
+}

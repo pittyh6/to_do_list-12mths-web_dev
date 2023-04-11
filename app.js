2dd829af09328675c618e37cf6246f4bc11be98c
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2023-04-11 13:55:44 +1000

    public/css/ insert index.css and animation.css inside the folder public/css

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html")
 })
